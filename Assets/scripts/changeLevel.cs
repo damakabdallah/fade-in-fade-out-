f3f2871fdf5a74f9ad3ae6e5e5741316a9f29505
Damak Abdallah <xttrzax@gmail.com>
2018-07-29 10:17:45 +0100

    first commit of unity project

---
+++ Assets/scripts/changeLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeLevel : MonoBehaviour {
+
+    public Animator animator;
+    static bool onlyOnce=false;
+    void Awake()
+    {
+        //working only once when launching the game
+        if (onlyOnce == false)
+            onlyOnce = true;
+        //working all the rest of time
+        else
+            animator.SetTrigger("fadeIn");
+    }
+    public void changeScene (string sceneName)
+    {
+        animator.SetTrigger("fadeOut");
+        StartCoroutine(waitUntilFadeEnded(sceneName));
+    }
+    IEnumerator waitUntilFadeEnded(string sceneName)
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+    }
+}
